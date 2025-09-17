d864f8ef770865f68919a3f6e9bde2339a848981
im-darkrai <mjha1422@gmail.com>
2025-09-17 14:24:38 +0530

    Bind Investigating

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 let h1= document.querySelector("h1")
 h1.innerText =" Mai hoon TheOGReturns : ";
 
-let h2 = document.createElement("h2");
-h2.textContent="Hello Hero Plays";
+// let h2 = document.createElement("h2");
+// h2.textContent="Hello Hero Plays";
 
-document.body.append(h2);
+// document.body.append(h2);
 
 
-h2.style.color = "red";
+// h2.style.color = "red";
 
 let b1= document.createElement("button");
 b1.textContent = " Buy Now: ";
@@ -19,4 +19,58 @@ b1.addEventListener("mouseleave", () => {
   b1.style.background = "white";
   b1.style.color ="black"
 });
+b1.addEventListener("click", () =>{
+    b1.style.transform = "scale(2.5)";
+});
 document.body.append(b1);
+
+console.dir("Hello world");
+
+
+// Page par koi bhi click karenge to events raise ho jaati hai . -->
+
+
+// Event Listener ka role hota hai kisi event ke reaction ke appropiate particular reaction dena..
+
+/*
+
+Sabse pahle tag select karte hai 
+let h1 = document.querySelector("Tag Name");
+
+2nd step -->
+
+addEventListener
+h1.add EventListener ("Event", function(){
+css, js .. etc
+})
+ */
+
+let h2 = document.querySelector("h2");
+h2.addEventListener("click",function(){
+  h2.style.color ="red";
+})
+
+let inpt = document.querySelector("input")
+inpt.addEventListener("input", function(data){
+  if(data!== null){
+  console.log(data);
+  }
+})
+
+//Investigating call bind 
+
+const user = {
+  name : `asha`,
+  greet (){
+    console.log(this.name);
+    
+  }
+}
+user.greet();
+const f = user.greet;
+f.call(user);
+const bound = f.bind(user);
+ bound();
+
+
+ 
