01d529d38c7aa799815822c6f5a642d36051eb31
im-darkrai <mjha1422@gmail.com>
2025-09-21 12:26:45 +0530

    Sabko Folder me daal Diya

---
+++ PractiseSet/Conversion.js
@@ -0,0 +1,13 @@
+let score = 85;
+console.log(score);
+
+console.log(typeof score);
+
+let Str = "85";
+
+Str = score;
+
+console.log(Str);
+
+console.log(typeof Str);
+
---
+++ PractiseSet/script.js
@@ -0,0 +1,6 @@
+let h1 = document.querySelector("h1");
+
+h1.style.color ="red";
+let para = document.createElement ("h2");
+para.textContent = "hello banty mai yaha hu";
+document.body.prepend(para);
